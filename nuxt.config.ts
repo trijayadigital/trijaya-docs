48115c00058e4ba609b956c44a4337ed821ba7f2
PT Trijaya Digital Grup <terusberkreasi@gmail.com>
2025-10-02 21:53:45 +0700

    add: add introduction and account pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,17 @@
 export default defineNuxtConfig({
   modules: [
-    '@nuxt/ui', 
+    '@nuxt/ui',
     '@nuxtjs/color-mode'
   ],
   css: ['./app/assets/css/main.css'],
 
   ui: {
     colorMode: false
+  },
+
+  nitro: {
+    routeRules: {
+      '/': { redirect: '/introduction/overview' }
+    }
   }
 });
